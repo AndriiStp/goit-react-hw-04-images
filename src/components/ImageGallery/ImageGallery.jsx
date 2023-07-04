674dcf40c3dd71d8abebcd3db7a365763aea7050
Andrii Stepanenko <andrii.stp@gmail.com>
2023-07-04 22:41:12 +0300

    gallery done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,79 +1,74 @@
-import { useState, udeEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Loader from 'components/Loader/Loader';
 import css from './imageGallery.module.css';
 
 const ImageGallery = ({ searchQuery, page, onImagesData }) => {
-
   const [images, setImages] = useState([]);
   const [status, setStatus] = useState('idle');
 
-  componentDidUpdate(prevProps) {
-    
-    if (prevProps.searchQuery !== searchQuery || prevProps.page !== page) {
-      this.fetchImages(searchQuery, page);
-    }
-  }
+  useEffect(() => {
+    const fetchImages = async (searchQuery, page) => {
+      setStatus('pending');
 
-  const fetchImages = (searchQuery, page) => {
-    setStatus('pending');
+      try {
+        const response = await fetch(
+          `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=36119540-6b0ed103a080a17c105931ea0&image_type=photo&orientation=horizontal&per_page=12`
+        );
 
-    fetch(
-      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=36119540-6b0ed103a080a17c105931ea0&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(response => {
         if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Error fetching images.');
-      })
-      .then(imagesData => {
-        const { hits, totalHits } = imagesData;
+          const imagesData = await response.json();
+          const { hits, totalHits } = imagesData;
 
-        if (page === 1) {
-          setImages(hits);
-          setStatus('resolved')
+          if (page === 1) {
+            setImages(hits);
+            setStatus('resolved');
+          } else {
+            setImages(prevImages => [...prevImages, ...hits]);
+            setStatus('resolved');
+          }
+
+          onImagesData(hits, totalHits);
         } else {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...hits],
-            status: 'resolved',
-          }));
+          throw new Error('Error fetching images.');
         }
-
-        onImagesData(hits, totalHits);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error(error);
         setStatus('rejected');
-      });
-  };
+      }
+    };
 
-    if (status === 'idle') {
-      return null;
+    if (searchQuery !== '') {
+      fetchImages(searchQuery, page);
     }
+  }, [searchQuery, page, onImagesData]);
 
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return <h1>Error, please try again later!</h1>;
-    }
-
-    return (
-      <ul className={css.image__gallery}>
-        {images.map(({ id, tags, webformatURL, largeImageURL }) => (
-          <ImageGalleryItem
-            key={id}
-            alt={tags}
-            webformatURL={webformatURL}
-            largeImageURL={largeImageURL}
-          />
-        ))}
-      </ul>
-    );
+  if (status === 'idle') {
+    return null;
   }
+
+  if (status === 'pending') {
+    return <Loader />;
+  }
+
+  if (status === 'rejected') {
+    return <h1>Error, please try again later!</h1>;
+  }
+
+  return (
+    <ul className={css.image__gallery}>
+      {images.map(({ id, tags, webformatURL, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          alt={tags}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+        />
+      ))}
+    </ul>
+  );
+};
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string.isRequired,
