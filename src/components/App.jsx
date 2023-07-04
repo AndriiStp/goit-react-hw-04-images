035883489aa69121a8b8edf15125c24ea2d3b490
Andrii Stepanenko <andrii.stp@gmail.com>
2023-07-04 23:47:03 +0300

    full)))

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,43 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import Searchbar from 'components/Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import css from './App.module.css';
 
-export default class App extends Component {
-  state = {
-    searchQuery: '',
-    page: 1,
-    totalHits: 0,
-    images: [],
-  };
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [images, setImages] = useState([]);
 
-  handleSearchSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1, images: [] });
-  };
+  const handleSearchSubmit = useCallback(searchQuery => {
+    setPage(1);
+    setImages([]);
+    setSearchQuery(searchQuery);
+  }, []);
 
-  handleImagesData = (hits, totalHits) => {
-    this.setState(prevState => ({
-      images: [...prevState.images, ...hits],
-      totalHits,
-    }));
-  };
+  const handleImagesData = useCallback((hits, totalHits) => {
+    setImages(prevImages => [...prevImages, ...hits]);
+    setTotalHits(totalHits);
+  }, []);
 
-  handleLoadMore = () => {
-    const { searchQuery, page } = this.state;
-    this.setState({ page: page + 1 });
-    this.fetchImages(searchQuery, page + 1);
-  };
+  const handleLoadMore = useCallback(() => {
+    setPage(prevPage => prevPage + 1);
+  }, []);
 
-  fetchImages = (searchQuery, page) => {
-    fetch(
-      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=36119540-6b0ed103a080a17c105931ea0&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(imagesData => {
-        const { hits, totalHits } = imagesData;
-        this.setState(prevState => ({
-          images: [...prevState.images, ...hits],
-          totalHits,
-        }));
-      });
-  };
+  const moreContent = images.length < totalHits && images.length > 0;
 
-  render() {
-    const { searchQuery, page, totalHits, images } = this.state;
-    const moreContent = images.length < totalHits && images.length > 0;
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleSearchSubmit} />
+      <ImageGallery
+        searchQuery={searchQuery}
+        page={page}
+        onImagesData={handleImagesData}
+      />
+      {moreContent && <Button handleLoadMore={handleLoadMore} />}
+    </div>
+  );
+};
 
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleSearchSubmit} />
-        <ImageGallery
-          searchQuery={searchQuery}
-          page={page}
-          onImagesData={this.handleImagesData}
-        />
-        {moreContent && <Button handleLoadMore={this.handleLoadMore} />}
-      </div>
-    );
-  }
-}
+export default App;
